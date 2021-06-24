ba3f4f3108e3a31474607531cff26943bcc9fbe1
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2021-06-23 22:37:38 -0400

    Initial Commit

---
+++ src/main/java/com/course/coursesubscription/CourseSubscriptionApplication.java
@@ -0,0 +1,20 @@
+package com.course.coursesubscription;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSubscriptionApplication implements CommandLineRunner {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSubscriptionApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		String a = "I remember seeing in either IntellsissssszzzzzzzzzzzzzzzzzzzzzzzzJ or Eclipse the sssetting to " +
+				"reformsat (cleanup) fildes " + "whenever thñey are saved. How do I find it (didn't find it in the " +
+				"settingsss)";
+	}
+}
---
+++ src/test/java/com/course/coursesubscription/CourseSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.coursesubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
