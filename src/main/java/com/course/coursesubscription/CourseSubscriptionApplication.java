9653ad26772905734cc10f059c25e04aae36d791
luisorellana777 <luis.orellana777@gmail.com>
2021-06-24 22:50:02 -0400

    Update CourseSubscriptionApplication.java

--- src/main/java/com/course/coursesubscription/CourseSubscriptionApplication.java
+++ src/main/java/com/course/coursesubscription/CourseSubscriptionApplication.java
@@ -2,7 +2,9 @@ package com.course.coursesubscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CourseSubscriptionApplication {
 
